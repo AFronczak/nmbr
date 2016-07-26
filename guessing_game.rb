b52e00c826cff9329d2b6f4c6bb8010a663dc9fa
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-07-26 15:06:35 -0400

    Random Number generator created

---
+++ guessing_game.rb
@@ -0,0 +1,12 @@
+# [ ] Pick a random number between 1 and 100
+# [ ] Prompt you for a guess
+# [ ] If your guess is less than the program's number, it should tell you that you were low and let you guess again.
+# [ ] If the guess is greater than the program's number, it should tell you that you were high and let you guess again.
+# [ ] If your guess is correct, the program should tell you that you win and then quit.
+# [ ] After 5 incorrect guesses, the program should tell you that you lose.
+# [ ] Your code should use at least two methods. NOTE this doesn't say in a class -- but if you feel like there is a good data+behavior approach, go for it.
+# [ ] This game should be run from the command line by typing "ruby guessing_game.rb".
+
+number = rand(100)
+
+p number
